f440b3a3061098549ca5d660a333f8fcc506ea80
max <max@thousandyardstare.de>
2021-02-06 12:23:40 +0100

    Move card generation code to generator

--- cardgenerators/gen_first_sent.py
+++ cardgenerators/gen_first_sent.py
@@ -3,11 +3,36 @@ import genanki
 _NAME = "Single Sentence Generator"
 _DESCRIPTION = """This generator only uses the first chosen sentence for a simple two-way cards. The others get ignored."""
 
+fields = [
+    {"name": "sentence"},
+    {"name": "definition"},
+]
+
+multisentence_model = genanki.Model(
+    1108891939,
+    "Simpel Sentence Card",
+    fields=fields,
+    templates=[
+        {
+            "name": "One-to-One Card",
+            "qfmt": "{{sentence}} {{tts fr_FR speed=1.25 :sentence}}",
+            "afmt": '{{FrontSide}}<hr id="answer">{{definition}} {{tts fr_FR speed=1.25 :definition}}',
+        }
+    ],
+)
+
 def generate_notes(card_models):
     """Takes a bunch of card models (see XXX) and returns a list of genanki notes"""
+    notes = []
+    
     for card in card_models:
-        field_definition = card["definition"]
+        first_sent = card["sentences"]
+        vocab_definition = card["definition"]
 
         note_basic = genanki.Note(
-            model=multisentence_model, fields=[*card["sentences"], field_definition]
+            model=multisentence_model, fields=[first_sent, vocab_definition]
         )
+
+        notes.append(note_basic)
+    
+    return notes
--- screens/screen_card.py
+++ screens/screen_card.py
@@ -26,31 +26,6 @@ class CardScreen(ScreenWithState):
         )
 
     def update_gui_contents(self):
-        fields = [
-            {"name": "sentence-0"},
-            {"name": "sentence-1"},
-            {"name": "sentence-2"},
-            {"name": "sentence-3"},
-            {"name": "sentence-4"},
-            {"name": "definition"},
-        ]
-
-        multisentence_model = genanki.Model(
-            1667392319,
-            "Some Model from epub2anki",
-            fields=fields,
-            templates=[
-                {
-                    "name": f"One-To-Many Cards, Sentence {isent}",
-                    "qfmt": "{{FIELDNAME}} {{tts fr_FR speed=1.25 :FIELDNAME}}".replace(
-                        "FIELDNAME", sentence_field["name"]
-                    ),
-                    "afmt": '{{FrontSide}}<hr id="answer">{{definition}} {{tts fr_FR speed=1.25 :definition}}',
-                }
-                for isent, sentence_field in enumerate(fields)
-            ],
-        )
-
         epub_path = self._state["epub_paths"][0]
         deck_name = os.path.basename(epub_path)
         book_deck = genanki.Deck(2059400110, deck_name)
